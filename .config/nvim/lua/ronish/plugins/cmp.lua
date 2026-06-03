6931f1ab625fd394be38871434c2713c40568b55
ronishmaharjan <ronish-maharjan@users.noreply.github.com>
2026-06-03 15:08:08 +0545

    update(lsp): add file path plugin

--- .config/nvim/lua/ronish/plugins/cmp.lua
+++ .config/nvim/lua/ronish/plugins/cmp.lua
@@ -5,6 +5,7 @@ return {
             "hrsh7th/cmp-nvim-lsp",
             "L3MON4D3/LuaSnip",
             "saadparwaiz1/cmp_luasnip",
+            "hrsh7th/cmp-path",
             "rafamadriz/friendly-snippets",
         },
 
